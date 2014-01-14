c6fb2033637260a76f8dbe2dce82c54f7136d788
DHunt <DHunt@glgroup.com>
2014-01-14 10:44:30 +0000

    WIP

--- index.js
+++ index.js
@@ -4,22 +4,52 @@ var path = require('path');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var File = gutil.File;
+var sha1 = require('sha1');
+
 
 module.exports = function(fileName, opt){
   if (!fileName) throw new PluginError('gulp-hash',  'Missing fileName option for gulp-hash');
   if (!opt) opt = {};
   if (!opt.newLine) opt.newLine = gutil.linefeed;
 
-  var buffer = [];
+  var objects = [];
+    var firstFile = null;
 
   function bufferContents(file){
     if (file.isNull()) return; // ignore
     if (file.isStream()) return this.emit('error', new PluginError('gulp-hash',  'Streaming not supported'));
+    //console.log(JSON.stringify(file));
+    //console.log(file.path);
 
-    buffer.push(file.contents.toString('utf8'));
+    if (!firstFile) firstFile = file;
+
+    var extName = path.extname(file.path);
+    var fileName = path.basename(file.path, extName);
+    var hash = sha1(fileName);
+
+
+    obj = {
+        base: file.base,
+        path: file.path,
+        filename: fileName,
+        extension: extName,
+        newFilename: fileName + hash + extName 
+    };
+
+    objects.push(obj);
   }
 
   function endStream(){
+    //console.log(JSON.stringify(objects));  
+
+    var assetsFile = new File({
+        cwd: firstFile.cwd,
+        base: firstFile.base,
+        path: path.join(firstFile.base, fileName),
+        contents: new Buffer(JSON.stringify(objects))   
+    }); 
+
+    this.emit('data', assetsFile);
     this.emit('end');
   }
 
